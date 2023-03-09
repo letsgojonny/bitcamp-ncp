72345af9141c0a657fb606aa860088ed81828e7d
letsgojonny <kjh0206sky@naver.com>
2023-03-09 20:26:05 +0900

    myapp-Client Rendering

--- myapp/app-server/src/main/java/bitcamp/myapp/controller/AuthController.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/controller/AuthController.java
@@ -1,20 +1,21 @@
 package bitcamp.myapp.controller;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import bitcamp.myapp.service.StudentService;
 import bitcamp.myapp.service.TeacherService;
 import bitcamp.myapp.vo.Member;
+import bitcamp.util.RestResult;
+import bitcamp.util.RestStatus;
 
-@Controller
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
 
   Logger log = LogManager.getLogger(getClass());
@@ -26,36 +27,12 @@ public class AuthController {
   @Autowired private StudentService studentService;
   @Autowired private TeacherService teacherService;
 
-  @RequestMapping("/auth/form")
-  public void form(@CookieValue(required = false) String email,
-      Model model,
-      HttpSession session) {
-    model.addAttribute("email", email);
-    if (session.getAttribute("error") != null) {
-      model.addAttribute("error", session.getAttribute("error"));
-    }
-  }
-
-  @RequestMapping("/auth/login")
-  public String login(
+  @PostMapping("login")
+  public Object login(
       String usertype,
       String email,
       String password,
-      String saveEmail,
-      HttpServletResponse response,
-      HttpSession session,
-      Model model) {
-
-    if (saveEmail != null) {
-      Cookie cookie = new Cookie("email", email);
-      cookie.setMaxAge(60 * 60 * 24 * 30); // 30일 동안 유지
-      response.addCookie(cookie);
-
-    } else {
-      Cookie cookie = new Cookie("email", "");
-      cookie.setMaxAge(0);
-      response.addCookie(cookie);
-    }
+      HttpSession session) {
 
     Member member = null;
     switch (usertype) {
@@ -69,23 +46,33 @@ public class AuthController {
 
     if (member != null) {
       session.setAttribute("loginUser", member);
-      session.removeAttribute("error");
-      return "redirect:../../";
+      return new RestResult()
+          .setStatus(RestStatus.SUCCESS);
     } else {
-      session.setAttribute("error", "loginfail");
-      return "redirect:form";
+      return new RestResult()
+          .setStatus(RestStatus.FAILURE);
     }
 
   }
 
-  @RequestMapping("/auth/logout")
-  public String logout(HttpSession session) {
+  @GetMapping("logout")
+  public Object logout(HttpSession session) {
     session.invalidate();
-    return "redirect:../../";
+    return new RestResult()
+        .setStatus(RestStatus.SUCCESS);
   }
 
-  @RequestMapping("/auth/fail")
-  public void fail() {
+  @RequestMapping("user")
+  public Object user(HttpSession session) {
+    Member loginUser = (Member) session.getAttribute("loginUser");
+    if (loginUser != null) {
+      return new RestResult()
+          .setStatus(RestStatus.SUCCESS)
+          .setData(loginUser);
+    } else {
+      return new RestResult()
+          .setStatus(RestStatus.FAILURE);
+    }
   }
 
 }
--- myapp/app-server/src/main/java/bitcamp/myapp/controller/BoardController.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/controller/BoardController.java
@@ -2,28 +2,27 @@ package bitcamp.myapp.controller;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import bitcamp.myapp.service.BoardService;
 import bitcamp.myapp.vo.Board;
 import bitcamp.myapp.vo.BoardFile;
 import bitcamp.myapp.vo.Member;
+import bitcamp.util.ErrorCode;
+import bitcamp.util.RestResult;
+import bitcamp.util.RestStatus;
 
-@Controller
+@RestController
 @RequestMapping("/board")
 public class BoardController {
 
@@ -33,21 +32,13 @@ public class BoardController {
     log.trace("BoardController 생성됨!");
   }
 
-  // ServletContext 는 요청 핸들러의 파라미터로 주입 받을 수 없다.
-  // 객체의 필드로만 주입 받을 수 있다.
   @Autowired private ServletContext servletContext;
   @Autowired private BoardService boardService;
 
-  @GetMapping("form")
-  public void form() {
-  }
-
   @PostMapping("insert")
-  @ResponseBody
   public Object insert(
       Board board,
       List<MultipartFile> files,
-      Model model,
       HttpSession session) throws Exception{
 
     Member loginUser = (Member) session.getAttribute("loginUser");
@@ -75,43 +66,51 @@ public class BoardController {
 
     boardService.add(board);
 
-    Map<String,Object> result = new HashMap<>();
-    result.put("status", "success");
-
-    return "success";
+    return new RestResult()
+        .setStatus(RestStatus.SUCCESS);
   }
 
   @GetMapping("list")
-  @ResponseBody
-  public Object list(String keyword, Model model) {
+  public Object list(String keyword) {
     log.debug("BoardController.list() 호출됨!");
-    List<Board> boards = boardService.list(keyword);
 
     // MappingJackson2HttpMessageConverter 가 jackson 라이브러리를 이용해
     // 자바 객체를 JSON 문자열로 변환하여 클라이언트로 보낸다.
     // 이 컨버터를 사용하면 굳이 UTF-8 변환을 설정할 필요가 없다.
     // 즉 produces = "application/json;charset=UTF-8" 를 설정하지 않아도 된다.
-    return boards;
+    return new RestResult()
+        .setStatus(RestStatus.SUCCESS)
+        .setData(boardService.list(keyword));
   }
 
   @GetMapping("view")
-  @ResponseBody
-  public Object view(int no, Model model) {
-    return boardService.get(no);
+  public Object view(int no) {
+    Board board = boardService.get(no);
+    if (board != null) {
+      return new RestResult()
+          .setStatus(RestStatus.SUCCESS)
+          .setData(board);
+    } else {
+      return new RestResult()
+          .setStatus(RestStatus.FAILURE)
+          .setErrorCode(ErrorCode.rest.NO_DATA);
+    }
   }
 
   @PostMapping("update")
-  public String update(
+  public Object update(
       Board board,
       List<MultipartFile> files,
-      Model model,
       HttpSession session) throws Exception {
 
     Member loginUser = (Member) session.getAttribute("loginUser");
 
     Board old = boardService.get(board.getNo());
     if (old.getWriter().getNo() != loginUser.getNo()) {
-      return "redirect:../auth/fail";
+      return new RestResult()
+          .setStatus(RestStatus.FAILURE)
+          .setErrorCode(ErrorCode.rest.UNAUTHORIZED)
+          .setData("권한이 없습니다.");
     }
 
     List<BoardFile> boardFiles = new ArrayList<>();
@@ -134,37 +133,44 @@ public class BoardController {
 
     boardService.update(board);
 
-    return "board/update";
+    return new RestResult()
+        .setStatus(RestStatus.SUCCESS);
   }
 
   @PostMapping("delete")
-  public String delete(int no, Model model, HttpSession session) {
+  public Object delete(int no, HttpSession session) {
     Member loginUser = (Member) session.getAttribute("loginUser");
 
     Board old = boardService.get(no);
     if (old.getWriter().getNo() != loginUser.getNo()) {
-      return "redirect:../auth/fail";
+      return new RestResult()
+          .setStatus(RestStatus.FAILURE)
+          .setErrorCode(ErrorCode.rest.UNAUTHORIZED)
+          .setData("권한이 없습니다.");
     }
     boardService.delete(no);
 
-    return "board/delete";
+    return new RestResult()
+        .setStatus(RestStatus.SUCCESS);
   }
 
-  @GetMapping("filedelete")
-  public String filedelete(int boardNo, int fileNo, HttpSession session) {
+  @PostMapping("filedelete")
+  public Object filedelete(int boardNo, int fileNo, HttpSession session) {
     Member loginUser = (Member) session.getAttribute("loginUser");
     Board old = boardService.get(boardNo);
+
     if (old.getWriter().getNo() != loginUser.getNo()) {
-      return "redirect:../auth/fail";
+      return new RestResult()
+          .setStatus(RestStatus.FAILURE)
+          .setErrorCode(ErrorCode.rest.UNAUTHORIZED)
+          .setData("권한이 없습니다.");
     } else {
       boardService.deleteFile(fileNo);
-      return "redirect:view?no=" + boardNo;
+      return new RestResult()
+          .setStatus(RestStatus.SUCCESS);
     }
   }
-
 }
-
-
 
 
 
