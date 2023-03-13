b56611693b8d19d59830ac0c5423dc7d16da0159
letsgojonny <kjh0206sky@naver.com>
2023-03-13 20:33:09 +0900

    myapp-client rendering

--- myapp/app-server/src/main/java/bitcamp/myapp/controller/StudentController.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/controller/StudentController.java
@@ -3,18 +3,21 @@ package bitcamp.myapp.controller;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import bitcamp.myapp.service.StudentService;
 import bitcamp.myapp.vo.Student;
 import bitcamp.util.RestResult;
 import bitcamp.util.RestStatus;
 
-@Controller
+@RestController
+@RequestMapping("/students")
 public class StudentController {
 
   Logger log = LogManager.getLogger(getClass());
@@ -25,43 +28,46 @@ public class StudentController {
 
   @Autowired private StudentService studentService;
 
-  @GetMapping("/student/form")
-  public void form() {
-  }
-
-  @PostMapping("/student/insert")
-  public void insert(Student student, Model model) {
+  @PostMapping
+  public Object insert(@RequestBody Student student) {
     studentService.add(student);
+    return new RestResult()
+        .setStatus(RestStatus.SUCCESS);
   }
 
-  @GetMapping("/students")
-  @ResponseBody
-  public Object list(String keyword, Model model) {
+  @GetMapping
+  public Object list(String keyword) {
     return new RestResult()
         .setStatus(RestStatus.SUCCESS)
         .setData(studentService.list(keyword));
   }
 
-  @GetMapping("/students/{no}")
-  @ResponseBody
+  @GetMapping("{no}")
   public Object view(@PathVariable int no) {
     return new RestResult()
         .setStatus(RestStatus.SUCCESS)
         .setData(studentService.get(no));
   }
 
-  @PostMapping("/students")
-  @ResponseBody
-  public Object update(Student student) {
+  @PutMapping("{no}")
+  public Object update(
+      @PathVariable int no,
+      @RequestBody Student student) {
+
+    log.debug(student);
+
+    // 보안을 위해 URL 번호를 게시글 번호로 설정한다.
+    student.setNo(no);
+
     studentService.update(student);
     return new RestResult()
         .setStatus(RestStatus.SUCCESS);
   }
 
-  @PostMapping("/student/delete")
-  public void delete(int no, Model model) {
+  @DeleteMapping("{no}")
+  public Object delete(@PathVariable int no) {
     studentService.delete(no);
+    return new RestResult()
+        .setStatus(RestStatus.SUCCESS);
   }
-
-
 }
